987682762ee5777860a2313d48d40eab9625d948
Amogh Anshu N <106390490+amoghanshu@users.noreply.github.com>
2024-02-22 12:25:12 +0530

    Update reverse.asm

--- reverse.asm
+++ reverse.asm
@@ -1,26 +1,24 @@
 .model small
 .data
-x db "UVCE"
-z dw ($-x)
-y db ($-x) dup (?), '$'
+str db 'UVCE', '$'
+len equ $-str-1
 .code
-mov ax, @data ;[ initialize
-mov ds, ax ; data segment]
-mov es, ax
-lea si, x ; get the address of x in si
-add si, z ;[si]+[z]→si
-dec si ; decrement si
-lea di, y ; get the address of y in di
-mov cx, z ; copy contents of z to cx reg
-L1: mov al, [si] ; contents of si moved to di reg
-mov [di], al
-dec si ; decrement si
-inc di ; increment di
-dec cx ; decrement cx reg
-jnz L1 ; if [cx]#0,jump to L1
-mov dx, offset y ; get address of y in dx reg
-mov ah, 09h ;display y
+mov ax, @data
+mov ds, ax
+mov si, offset str
+mov di, offset str + len - 1
+mov cx, len / 2
+reverse_loop:
+    mov al, [si]
+    mov ah, [di]
+    mov [si], ah
+    mov [di], al
+    inc si
+    dec di
+    loop reverse_loop
+mov dx, offset str
+mov ah, 09h
 int 21h
-mov ah, 4ch ; [ terminate
-int 21h ; exit to DOS]
+mov ah, 4ch
+int 21h
 end
